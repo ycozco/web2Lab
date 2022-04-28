ef2d2b2859fc1db5595770bef788bf718caf54a0
yoset <ycozco@unsa.edu.pe>
2022-04-27 19:10:32 -0500

    agregando la funcion suma

--- calculator.java
+++ calculator.java
@@ -2,6 +2,11 @@ import java.util.*;
 
 public class calculator {
     public static void main(String args[]) {
-
+        // functions suma, resta, multiplicacion, division
     }
+
+    double suma(double a, double b) {
+        return a + b;
+    }
+
 }
